c39d6ecb1d6fb38a94f64187d00ee6286cdef1fc
Елизавета Голубева <elizavetagolubeva@MacBook-Air-Elizaveta.local>
2022-03-28 16:26:11 +0300

    Initial commit

---
+++ src/com/company/hwCommand.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class hwCommand {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);//создаем объект сканер
+        String text = scanner.nextLine();//ввод с клавы ко
+        if (text.equals("hello")) {
+            System.out.println("Hello, world!");
+        }
+    }
+}
+
+
